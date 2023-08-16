1552392e581b63c7a1420f67888aea863a2956dd
Piyush Jain <piyushjain5350@gmail.com>
2023-08-16 07:54:08 +0530

    1 solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[isLoggedInd,setIsLoggedIn]=useState(false);
+
+  function SubmitFn(){
+    setIsLoggedIn(true);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        {!isLoggedInd&&<form onSubmit={SubmitFn}>
+          <label htmlFor="nameInput">Username:</label>
+          <input type="text" name="nameInput"/><br />
+          <label htmlFor="passwordInput">Password: </label>
+          <input type="password" name="passwordInput"/>
+          <br />
+          <input type="submit" value="Login" />
+        </form>}
+        {
+          isLoggedInd&&<p>You are logged in!</p>
+        }
     </div>
   )
 }
